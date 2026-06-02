9068f48aa4a6d4c6015bd1d1acb2f32a6c89b874
Funti3r <katlego.h@funti3r.xyz>
2026-06-02 16:06:27 +0200

    fix: use exact Cloud Run domain in ALLOWED_HOSTS

--- gtm_validator/settings.py
+++ gtm_validator/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-c=&nz9k*2q%_@ft=n_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,*.run.app").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,gtm-validator-601175512678.us-west1.run.app").split(",")
 
 
 # Application definition
